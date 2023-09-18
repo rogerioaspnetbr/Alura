bbe22bd9edcbf03a938952edfd27155046444666
Rogerio Cardoso <rogeriocardososp@outlook.com>
2023-09-18 16:42:36 -0300

    teste 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarPensamentoComponent
   ],
   imports: [
     BrowserModule,
